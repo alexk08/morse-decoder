4d76771844f5778b85a257d3099bccff279d51f5
alexk08 <akapacuk@gmail.com>
2020-10-07 01:13:12 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = expr.split('');
+  let newArr = [];
+  for (let i = 0; i < arr.length/10; i++) {
+    const element = arr.slice(10*i, 10*i+10).join('');
+    newArr.push(element);
+  }
+  let decodeArr = [];
+  for (let i = 0; i < newArr.length; i++) {
+    const element = newArr[i];
+    let str ='';
+    if (element !== '**********') {
+      for (let j = element.length-1; j>=0; j-=2) {
+        const element1 = element.slice(j-1,j+1);
+        if (element1 === '11') {
+          str = '-' + str;
+        } else if (element1 === '10') {
+          str = '.' + str;
+        } else {
+          str = '' + str;
+        }
+      }
+      decodeArr.push(str);
+    } else {
+      decodeArr.push(' ');
+    }
+  }
+
+  let result = '';
+  for (let i = 0; i < decodeArr.length; i++) {
+    const element = decodeArr[i];
+    if (element !== ' ') {
+      result = result + MORSE_TABLE[element]
+    } else {
+      result = result + element;
+    }
+  }
+  return result;
 }
 
 module.exports = {
